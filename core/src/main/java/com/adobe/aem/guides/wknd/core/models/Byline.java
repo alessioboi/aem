53b952277e7412a2a5b450ea80f42b312197009c
alessioboi <boi.alessio@gmail.com>
2021-05-26 14:42:37 +0000

    Parte java

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Byline.java
@@ -0,0 +1,10 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import java.util.List;
+
+public interface Byline {
+String getName();
+List<String> getOccupations();
+boolean isEmpty();
+    
+}
